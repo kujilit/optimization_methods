c8569a65b52ad43e262aa3b569c352f1f44c395d
Mike Litvin <mrlitvinm@gmail.com>
2023-04-09 20:22:56 +1000

    calculated derivative of a function

--- Newtons_method/main.py
+++ Newtons_method/main.py
@@ -21,6 +21,23 @@ def lagrange_diff(x):
     return np.matmul(a, x) + b + 2 * y * (x - x_0)
 
 
+def diff(x):
+    return np.array([
+        [a + 2 * np.eye(4) * y, 2 * (x - x_0)],
+        [2 * (x - x_0).T, 0]
+    ])
+
+class Solution:
+    def __init__(self, x_0: np.array, a: np.array, eps):
+        self.a = a
+        self.x_prev = x_0
+        self.x_next = self.x_prev - np.linalg.inv(diff(self.x_prev)) * function(self.x_prev)
+
+        while error(self.x_next - self.x_prev) <= eps:
+            self.x_prev = self.x_next
+            self.x_next = self.x_prev - np.linalg.inv(diff(self.x_prev)) * function(self.x_prev)
+
+
 if __name__ == "__main__":
     a = np.array()
     b = np.array()
